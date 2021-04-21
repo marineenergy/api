48e7f957563e611c52bc52f12a4b3c937ece8820
Ben Best <ben@ecoquants.com>
2021-04-21 20:54:13 +0000

    + /ferc_docs /report with report_test.Rmd

--- plumber.R
+++ plumber.R
@@ -3,17 +3,69 @@ if (!require(librarian)){
   install.packages("librarian")
   library(librarian)
 }
-shelf(dplyr, tibble)
+shelf(dplyr, digest, fs, glue, here, purrr, rmarkdown, readr, stringr, tibble, tidyr)
 
-#* FERC docs in PRIMRE export metadata formate
+# TODO: https://rviews.rstudio.com/2019/08/13/plumber-logging/
+
+#* FERC docs in PRIMRE export metadata format
 #* @get /ferc_docs
 function() {
-  # TODO: add parameters, and search database with query to get results
-  records <- tribble(
-    ~URI, ~title, ~tags,
-    "https://mhk-env.us/ferc_docs/Cobscook-Bay-docA",   "Cobscook Bay doc A", "Marine Mammals; Noise",
-    "https://mhk-env.us/ferc_docs/Cobscook-Bay-docB",   "Cobscook Bay doc B", "Birds; Collision")
-  records
+  
+  dir_data <- here("../apps/data")
+  docs_csv <- glue("{dir_data}/ferc_docs2.csv")
+  
+  date_fetched <- file_info(docs_csv) %>% pull(change_time) %>% as.Date()
+  
+  docs_0 <- read_csv(docs_csv, col_types = cols()) %>% 
+    rename(doc_id = doc)
+  
+  docs <- docs_0 %>% 
+    select(doc_id, url, technology) %>% 
+    group_by(doc_id) %>% 
+    summarize(
+      url        = first(na.omit(url)), 
+      technology = first(na.omit(technology)), 
+      .groups    = "drop_last") %>%  # only 4 of 118 entries with url
+    mutate(
+      # TODO: doc_id without any special characters or spaces [a-Z_-]
+      URI               = glue("https://mhkdr.openei.org/data/{doc_id}"),
+      type              = "Document/Report",
+      landingPage       = URI,   # TODO: make pages
+      sourceURL         = url,
+      title             = doc_id,
+      description       = "TBD", # TODO: required field for PRIMRE
+      author            = map(NA, function(x) list("Maria Carnevale", "Sharon Kramer")), # TODO: add fields
+      organization      = "MarineEnergy.app",
+      originationDate   = as.Date("2021-11-07"), # TODO: handle date add/modify in future
+      spatial           = map(NA, function(x) 
+        list( # TODO: required fields lat,lon; default to global
+          extent                = "boundingBox",
+          boundingCoordinatesNE = c( 90,  180),
+          boundingCoordinatesSW = c(-90, -180))),
+      technologyType    = str_replace(technology, "Marine Energy.", ""),
+      signatureProject  = NA, # TODO: cross-list with Projects
+      modifiedDate      = date_fetched)
+    
+  doc_tags <- docs_0 %>% 
+    select(doc_id, receptor, stressor, phase) %>% 
+    mutate(
+      receptor = str_replace(receptor, fixed('.'), '/'),
+      stressor = str_replace(stressor, fixed('.'), '/')) %>% 
+    pivot_longer(-doc_id, names_to = "category", values_to = "tag") %>% 
+    filter(!is.na(tag)) %>% 
+    mutate(
+      tag = glue("{str_to_title(category)}/{tag}")) %>% 
+      select(-category) %>% 
+    group_by(doc_id) %>% 
+    nest(tags = c(tag)) %>% 
+    mutate(
+      tags = map(tags, function(x){ x %>% unlist %>% sort %>% unique}))
+   
+  docs <- docs %>% 
+    left_join(doc_tags, by = "doc_id") %>% 
+    select(-doc_id, -url, -technology)
+
+  docs
 }
 
 #* Echo back the input
@@ -29,6 +81,63 @@ function(msg="") {
 function() {
   rand <- rnorm(100)
   hist(rand)
+}
+
+#* Render report
+#* @param title Title
+#* @param receptors Receptors separated by a space (or comma or both)
+#* @get /report
+#* @html
+function(
+  req,
+  title,
+  receptors = NA,
+  ext       = "html",
+  res) {
+  # title    = "Test"; receptors = NULL
+
+  # paths
+  in_rmd  <- "report_test.Rmd"
+  dir_out <- "/share/api_out"
+  dir_url <- "https://marineenergy.app/api_out"
+  
+  args_hash <- digest(req$args)
+  
+  out_file   <- glue("{dir_out}/me_report_{args_hash}.{ext}")
+  out_url    <- glue("{dir_url}/{basename(out_file)}")
+  out_format <- c(
+    "html" = "html_document",
+    "pdf"  = "pdf_document",
+    "docx" = "word_document")[[ext]]
+  receptors  <- str_split(receptors, "[, ]+") %>% unlist()
+  
+  # TODO: handle refresh of same input args
+  if (!file.exists(out_file))
+    render(
+      input         = in_rmd, 
+      output_file   = out_file, 
+      output_format = out_format,
+      params = list(
+        title     = title,
+        receptors = receptors))
+  
+  stopifnot(file.exists(out_file))
+  
+  res$status <- 303 # redirect
+  res$setHeader("Location", out_url)
+  glue::glue(
+    "<html>
+    <head>
+      <meta http-equiv=\"Refresh\" content=\"0; url={out_url}\" />
+    </head>
+    <body>
+    </body>
+  </html>")
+}
+
+#* @html
+function(req, res) {
+  
 }
 
 #* Return the sum of two numbers
--- run_api.R
+++ run_api.R
@@ -1,10 +1,41 @@
 # run API by sourcing this R script in RStudio
 library(plumber)
+library(logger)
+library(tictoc)
 
 plumber_r <- "/share/github/api/plumber.R"
 
-pr(plumber_r) %>%
+pr <- pr(plumber_r) %>%
   pr_run(port=8888, host="0.0.0.0")
+
+# Specify how logs are written
+log_dir <- "logs"
+if (!fs::dir_exists(log_dir)) fs::dir_create(log_dir)
+log_appender(appender_tee(tempfile("plumber_", log_dir, "_log.txt")))
+
+convert_empty <- function(string) {
+  if (string == "") {
+    "-"
+  } else {
+    string
+  }
+}
+
+pr$registerHooks(
+  list(
+    preroute = function() {
+      # Start timer for log info
+      tictoc::tic()
+    },
+    postroute = function(req, res) {
+      end <- tictoc::toc(quiet = TRUE)
+      # Log details about the request and the response
+      log_info('{convert_empty(req$REMOTE_ADDR)} "{convert_empty(req$HTTP_USER_AGENT)}" {convert_empty(req$HTTP_HOST)} {convert_empty(req$REQUEST_METHOD)} {convert_empty(req$PATH_INFO)} {convert_empty(res$status)} {round(end$toc - end$tic, digits = getOption("digits", 5))}')
+    }
+  )
+)
+
+pr
 
 ## Reference
 #  - custom serializer: https://github.com/rstudio/plumber/issues/344#issuecomment-439492586
@@ -18,7 +49,7 @@ pr(plumber_r) %>%
 # for more, see https://www.rplumber.io/docs
 
 # To stop on rstudio.marinebon.app in Terminal:
-#   ps -eaf | grep mhk-env_api
+#   ps -eaf | grep api
 #   # admin      494   442  0 Feb07 pts/0    00:00:08 /usr/local/lib/R/bin/exec/R --no-save --no-restore --slave --no-restore --file=/srv/ws-api/run_api.R
 #   sudo kill -9 494
 # To start on rstudio.marinebon.app in Terminal:
