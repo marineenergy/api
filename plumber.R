2bc83b415e2d0ba495da5b6b4882ddff77db28ee
Ben Best <ben@ecoquants.com>
2021-05-10 21:18:31 +0000

    + htmlwidget, report API fxns

--- plumber.R
+++ plumber.R
@@ -1,16 +1,93 @@
 # plumber.R
-library(dplyr)
+if (!require(librarian)){
+  install.packages("librarian")
+  library(librarian)
+}
+shelf(dplyr, digest, fs, glue, here, highcharter, purrr, rmarkdown, readr, stringr, tibble, tidyr)
 
-#* FERC docs in PRIMRE export metadata formate
-#* @param msg The message to echo
+# TODO: https://rviews.rstudio.com/2019/08/13/plumber-logging/
+
+#' Plot the iris dataset using interactive chart
+#' 
+#' @param spec Species to filter
+#'
+#' @get /highchart
+#' @serializer htmlwidget
+function(spec){
+  library(highcharter)
+  
+  myData <- iris
+  title <- "All Species"
+  
+  # Filter if the species was specified
+  if (!missing(spec)){
+    title <- paste0("Only the '", spec, "' Species")
+    myData <- subset(iris, Species == spec)
+  }
+  
+  hchart(myData, "scatter", hcaes(x = Sepal.Length, y = Petal.Length, group = Species)) %>%
+    hc_title(text = title)
+}
+
+#* FERC docs in PRIMRE export metadata format
 #* @get /ferc_docs
-function(msg) {
-  # TODO: add parameters, and search database with query to get results
-  records <- tribble(
-    ~URI, ~title,
-    "https://mhk-env.us/ferc_docs/Cobscook-Bay-docA",   "Cobscook Bay doc A",
-    "https://mhk-env.us/ferc_docs/Cobscook-Bay-docB",   "Cobscook Bay doc B")
-  records
+function() {
+  
+  dir_data <- here("../apps/data")
+  docs_csv <- glue("{dir_data}/ferc_docs2.csv")
+  
+  date_fetched <- file_info(docs_csv) %>% pull(change_time) %>% as.Date()
+  
+  docs_0 <- read_csv(docs_csv, col_types = cols()) %>% 
+    rename(doc_id = doc)
+  
+  docs <- docs_0 %>% 
+    select(doc_id, url, technology) %>% 
+    group_by(doc_id) %>% 
+    summarize(
+      url        = first(na.omit(url)), 
+      technology = first(na.omit(technology)), 
+      .groups    = "drop_last") %>%  # only 4 of 118 entries with url
+    mutate(
+      # TODO: doc_id without any special characters or spaces [a-Z_-]
+      URI               = glue("https://mhkdr.openei.org/data/{doc_id}"),
+      type              = "Document/Report",
+      landingPage       = URI,   # TODO: make pages
+      sourceURL         = url,
+      title             = doc_id,
+      description       = "TBD", # TODO: required field for PRIMRE
+      author            = map(NA, function(x) list("Maria Carnevale", "Sharon Kramer")), # TODO: add fields
+      organization      = "MarineEnergy.app",
+      originationDate   = as.Date("2021-11-07"), # TODO: handle date add/modify in future
+      spatial           = map(NA, function(x) 
+        list( # TODO: required fields lat,lon; default to global
+          extent                = "boundingBox",
+          boundingCoordinatesNE = c( 90,  180),
+          boundingCoordinatesSW = c(-90, -180))),
+      technologyType    = str_replace(technology, "Marine Energy.", ""),
+      signatureProject  = NA, # TODO: cross-list with Projects
+      modifiedDate      = date_fetched)
+  
+  doc_tags <- docs_0 %>% 
+    select(doc_id, receptor, stressor, phase) %>% 
+    mutate(
+      receptor = str_replace(receptor, fixed('.'), '/'),
+      stressor = str_replace(stressor, fixed('.'), '/')) %>% 
+    pivot_longer(-doc_id, names_to = "category", values_to = "tag") %>% 
+    filter(!is.na(tag)) %>% 
+    mutate(
+      tag = glue("{str_to_title(category)}/{tag}")) %>% 
+    select(-category) %>% 
+    group_by(doc_id) %>% 
+    nest(tags = c(tag)) %>% 
+    mutate(
+      tags = map(tags, function(x){ x %>% unlist %>% sort %>% unique}))
+  
+  docs <- docs %>% 
+    left_join(doc_tags, by = "doc_id") %>% 
+    select(-doc_id, -url, -technology)
+  
+  docs
 }
 
 #* Echo back the input
@@ -26,6 +103,65 @@ function(msg="") {
 function() {
   rand <- rnorm(100)
   hist(rand)
+}
+
+#* Render report
+#* @param title Title
+#* @param receptors Receptors separated by a space (or comma or both)
+#* @get /report
+#* @html
+function(
+  req,
+  title,
+  receptors = NA,
+  ext       = "html",
+  res) {
+  # title    = "Test"; receptors = NULL
+  
+  # paths
+  in_rmd  <- "report_test.Rmd"
+  dir_out <- "/share/api_out"
+  dir_url <- "https://marineenergy.app/api_out"
+  
+  args_hash <- digest(req$args)
+  
+  browser()
+  
+  out_file   <- glue("{dir_out}/me_report_{args_hash}.{ext}")
+  out_url    <- glue("{dir_url}/{basename(out_file)}")
+  out_format <- c(
+    "html" = "html_document",
+    "pdf"  = "pdf_document",
+    "docx" = "word_document")[[ext]]
+  receptors  <- str_split(receptors, "[, ]+") %>% unlist()
+  
+  # TODO: handle refresh of same input args
+  if (!file.exists(out_file))
+    render(
+      input         = in_rmd, 
+      output_file   = out_file, 
+      output_format = out_format,
+      params = list(
+        title     = title,
+        receptors = receptors))
+  
+  stopifnot(file.exists(out_file))
+  
+  res$status <- 303 # redirect
+  res$setHeader("Location", out_url)
+  glue::glue(
+    "<html>
+    <head>
+      <meta http-equiv=\"Refresh\" content=\"0; url={out_url}\" />
+    </head>
+    <body>
+    </body>
+  </html>")
+}
+
+#* @html
+function(req, res) {
+  
 }
 
 #* Return the sum of two numbers
